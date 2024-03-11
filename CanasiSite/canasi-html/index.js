8f0afc114ab7bb718ea7f29f71a7aaf677687983
DatumCode <ldhymas44@gmail.com>
2024-03-10 20:12:13 -0600

    added rough quote implimentation

--- CanasiSite/canasi-html/index.js
+++ CanasiSite/canasi-html/index.js
@@ -6,14 +6,14 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.json());
 
-app.use(express.static('pulic'));
+app.use(express.static('public'));
 
 
 
 
 
 
-// Return the application's default page if the path is unknown
+// Return the application's default page if the path is unknown from simon
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
   });
@@ -26,9 +26,22 @@ app.use((_req, res) => {
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-//tracks wins and losses
+
+//Gets Wins and Losses
 let playerWins = 0;
 let playerLosses = 0;
+apiRouter.get('/playerWins', (_req, res) => {
+    //res.json({wins: playerWins});
+    res.send(playerWins);
+});
+
+apiRouter.post('/playerWins', (req, res) => {
+    playerWins = updateWinsLosses(req.body, playerWins)
+    res.send(playerWins);
+})
+
+//tracks wins and losses
+
 function updateWinsLosses(gameResult) {
     if (gameResult == "win") {
         playerWins++;
@@ -37,3 +50,30 @@ function updateWinsLosses(gameResult) {
     }
 
 }
+
+// this might be better
+/* // Initial values for wins and losses
+let playerWins = 0;
+let playerLosses = 0;
+
+// Get player wins
+apiRouter.get('/playerWins', (_req, res) => {
+    res.json({ wins: playerWins });
+});
+
+// Get player losses
+apiRouter.get('/playerLosses', (_req, res) => {
+    res.json({ losses: playerLosses });
+});
+
+// Log game result (win/loss)
+apiRouter.post('/logResult', (req, res) => {
+    const { result } = req.body;
+    if (result === "win") {
+        playerWins++;
+    } else if (result === "loss") {
+        playerLosses++;
+    }
+    res.sendStatus(200);
+});*/
+
--- CanasiSite/canasi-html/public/index.js
+++ CanasiSite/canasi-html/public/index.js
@@ -6,6 +6,29 @@ function login() {
 }
 
 
+// developed from simon 
+function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#Nykta-quote');
+  
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+  
+        quoteEl.textContent = data.content;
+        authorEl.textContent = data.author;
+  
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+      });
+  }
+
+  displayQuote();
+
+
     //const username = document.querySelector("#username");
     //localStorage.setItem("username", username.value);
 
